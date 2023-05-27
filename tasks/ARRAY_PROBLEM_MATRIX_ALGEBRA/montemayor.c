7b27c5664319517210ea36a4ac42295aa5923cbe
Keith Montemayor <114279473+kisuuuuu@users.noreply.github.com>
2023-05-27 16:31:51 +0800

    Add files via upload

---
+++ tasks/ARRAY_PROBLEM_MATRIX_ALGEBRA/montemayor.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+// Function to compute arrayc
+void compute(int arraya[3], int arrayb[3][3], int arrayc[3]);
+
+// Function to display arrays
+void display(int x[3], int y[3][3], int z[3]);
+
+int main() {
+    int arraya[3], arrayb[3][3];
+
+    // Input values for arraya
+    for (int i = 0; i < 3; i++) {
+        printf("Input arraya[%d]: ", i);
+        scanf("%d", &arraya[i]);
+    }
+
+    // Input values for arrayb
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("Input arrayb[%d][%d]: ", i, j);
+            scanf("%d", &arrayb[i][j]);
+        }
+    }
+
+    int arrayc[3];
+
+    // Compute arrayc
+    compute(arraya, arrayb, arrayc);
+
+    // Display arrays
+    display(arraya, arrayb, arrayc);
+}
+
+void compute(int arraya[3], int arrayb[3][3], int arrayc[3]) {
+    for (int i = 0; i < 3; i++) {
+        arrayc[i] = 0;
+        for (int j = 0; j < 3; j++) {
+            // Compute arrayc values by multiplying arraya and arrayb
+            arrayc[i] += arraya[j] * arrayb[j][i];
+        }
+    }
+}
+
+void display(int x[3], int y[3][3], int z[3]) {
+    printf("arraya = [%d, %d, %d]\n", x[0], x[1], x[2]);
+    printf("arrayb =\n ");
+    for (int i = 0; i < 3; i++) {
+        // Display arrayb values row-wise
+        printf("[%d, %d, %d]\n", y[i][0], y[i][1], y[i][2]);
+    }
+    printf("arrayc = [%d, %d, %d]\n", z[0], z[1], z[2]);
+}
